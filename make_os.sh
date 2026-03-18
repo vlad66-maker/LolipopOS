e43c2fb91301d5dc40f604c98f0e4307a76bd53f
fox4s <v3777627@gmail.com>
2026-03-18 14:33:05 +0300

    Add files via upload

---
+++ make_os.sh
@@ -0,0 +1,12 @@
+# 1. Собираем стартовый код
+nasm -f elf32 boot/bootloader.asm -o start.o
+
+# 2. Компилируем ядро на C
+gcc -m32 -ffreestanding -c kernel/main.c -o kernel.o
+gcc -m32 -ffreestanding -c kernel/keyboard.c -o keyboard.o
+
+# 3. Компонуем всё в ELF-файл. Обрати внимание на порядок: start.o должен быть первым.
+gcc -m32 -ffreestanding -T linker.ld -o myos.elf -nostdlib start.o kernel.o keyboard.o -lgcc
+mkdir -p iso/boot/grub
+cp myos.elf iso/boot/
+grub-mkrescue -o os.iso iso/
