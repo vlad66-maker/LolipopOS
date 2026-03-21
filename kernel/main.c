c620e25ea19c2d2ea8fb176dbe0368bfe2ec93d8
fox4s <v3777627@gmail.com>
2026-03-21 18:21:47 +0300

    main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,21 +1,6 @@
-#include <stdint.h>
-#include "keyboard.h"
-
-#define VIDEO ((char*)0xB8000)
-
-void print_at(const char *str, int x, int y, uint8_t color) {
-    char *video = VIDEO;
-    int offset = (y * 80 + x) * 2;
-    for (int i = 0; str[i] != '\0'; i++) {
-        video[offset + i*2] = str[i];
-        video[offset + i*2 + 1] = color;
-    }
-}
+#include "functions.h"
+#include "terminal.h"
 
 void kernel_main(void) {
-    print_at("LolipopOS", 0, 0, 0x0F);
-
-    while (1) {
-        keyboard_handler();      
-    }
+    terminal_init();
 }
