480a9800daa84f6fbee7f6ae2b18f22712416aee
fox4s <v3777627@gmail.com>
2026-03-30 21:02:42 +0300

    Update functions.h

--- kernel/functions.h
+++ kernel/functions.h
@@ -4,12 +4,13 @@
 #include <stdint.h>
 
 void     print_at(const char *str, int x, int y, uint8_t color);
-void     put_char(char c, int x, int y)                        ;
+void     put_char(char c, int x, int y, int8_t color)          ;
 uint8_t  inb(uint16_t port)                                    ;
 void     outb(uint16_t port, uint8_t data)                     ;
 void     strcpy(char *dest, const char *src)                   ;
 int8_t   strcmp(const char *str1, const char *str2)            ;
 void     clear_screen(uint8_t color)                           ;
 uint32_t strlen(const char *str)                               ;
+void     hlt(uint32_t ms)                                      ;
 
 #endif
